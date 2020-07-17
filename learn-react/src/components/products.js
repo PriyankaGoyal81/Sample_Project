c504611aa8f991b293ffe935ba5dfe4131648c67
Raghu Bedi <raghubedi2004@gmail.com>
2020-07-16 22:59:35 -0400

    Add all my files

---
+++ learn-react/src/components/products.js
@@ -0,0 +1,27 @@
+const products = [
+    {
+        "id": 1,
+        "name": "Raghu Bedi",
+        "city": "Toronto"
+    },
+    {
+        "id": 2,
+        "name": "Sachin Tendulkar",
+        "city": "Mumbai"
+    },
+    {
+        "id": 3,
+        "name": "Virat Kohli",
+        "city": "New Delhi"
+    },
+    {
+        "id": 4,
+        "name": "Priyanka Chopra",
+        "city": "New York"
+    },
+    {
+        "id": 1,
+        "name": "Mandeep Singh",
+        "city": "Melbourne"
+    },
+]
